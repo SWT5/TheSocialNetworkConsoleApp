823e12a7ee962b8159e3600f05f9f833ffb7c41f
Quynh Trang Mac <201810269@post.au.dk>
2020-05-04 17:11:52 +0200

    Added more to models

---
+++ TheSocialNetworkConsoleApp/Models/VideoPost.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheSocialNetworkConsoleApp.Models
+{
+    public class VideoPost:Post
+    {
+        public string Content { get; set; }
+        public Dictionary<string, int> Options { get; set; } = new Dictionary<string, int>(); // Ved ikke hvad det er....
+
+        public override void print()
+        {
+            Console.WriteLine("Write something here...:" + Content);
+        }
+    }
+}
