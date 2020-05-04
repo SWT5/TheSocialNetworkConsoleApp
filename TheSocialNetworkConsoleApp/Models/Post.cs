3ced0323a43a79d33895360a25b17fcf749b2079
Quynh Trang Mac <201810269@post.au.dk>
2020-05-04 16:57:44 +0200

    Added post model

---
+++ TheSocialNetworkConsoleApp/Models/Post.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TheSocialNetworkConsoleApp.Models;
+
+namespace TheSocialNetworkConsoleApp.Models
+{
+    class Post
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string PostID { get; set; }
+
+        public string Author { get; set; }
+        public User PostsAuthor { get; set; }
+
+        public DateTime CreationTime { get; set; }
+
+        public string PostInCircle { get; set; }
+        public Circle Circle { get; set; }
+
+        public List<Comments> CommentsList { get; set; }
+
+        public List<string> Content { get; set; }
+    }
+
+    public class Comments
+    {
+        public string Author { get; set; }
+        public User CommentsAuthor { get; set; }
+
+        public DateTime CommentCreationTime { get; set; }
+
+        public string Text { get; set; }
+    }
+}
