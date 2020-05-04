c89c251d94218e78012381611bbcced44acd1d1d
Thanh Tieu Pham <au479218@uni.au.dk>
2020-05-04 17:02:47 +0200

    comment model has been made

---
+++ TheSocialNetworkConsoleApp/Models/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TheSocialNetworkConsoleApp.Models
+{
+    class Comment
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CommentId { get; set; }
+        public string CommentText { get; set; }
+        
+    }
+}
