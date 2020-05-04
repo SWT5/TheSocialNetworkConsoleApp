3a946ecd2882cc849c670987a78e27c06ef3d4d1
Thanh Tieu Pham <au479218@uni.au.dk>
2020-05-04 17:29:10 +0200

    working on pragrame.cs

--- TheSocialNetworkConsoleApp/Program.cs
+++ TheSocialNetworkConsoleApp/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using MongoDB.Driver;
 
 namespace TheSocialNetworkConsoleApp
 {
     class Program
     {
+        static Services services = new Services();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // connects to a local database
+            Services._client = new MongoClient("mongodb://127.0.0.1:27017/");
+            Services._database = Services._client.GetDatabase("SocialNetworkDb");
+
+            services.CreateCollection();    //comment this out after first run of the programme
+            services.GetCollections();
+
+            Create create = new Create(services);
+
+            create.SeedingData();
         }
     }
 }
