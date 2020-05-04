19d1e5bfebf9179d03182a45df9514716057d5c7
Nikolaj Tam Binh Huynh <au591235@uni.au.dk>
2020-05-04 17:33:06 +0200

    Changed name of services folder to service

--- TheSocialNetworkConsoleApp/Service/Services.cs
+++ TheSocialNetworkConsoleApp/Service/Services.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using MongoDB.Driver;
-using TheSocialNetworkConsoleApp.Models;
 
 namespace TheSocialNetworkConsoleApp.Services
 {
