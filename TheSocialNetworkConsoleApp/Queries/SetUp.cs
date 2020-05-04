cc41bb9c63468e060d026ed1c78b6cb74adb668c
Mads Bo Mikkelsen <au615041@uni.au.dk>
2020-05-04 18:17:10 +0200

    setup addede constructor + property

--- TheSocialNetworkConsoleApp/Queries/SetUp.cs
+++ TheSocialNetworkConsoleApp/Queries/SetUp.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TheSocialNetworkConsoleApp.Services;
 
 namespace TheSocialNetworkConsoleApp.Queries
 {
-    class SetUp
+    public class SetUp
     {
+        public SetUp(Services.Services services)
+        {
+            _services = services;
+        }
+
+        public Services.Services _services { get; set; }
     }
 }
