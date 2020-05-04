8093f65d8770123ac3110da43364650719b972dd
Quynh Trang Mac <201810269@post.au.dk>
2020-05-04 17:29:22 +0200

    Added to pull

---
+++ TheSocialNetworkConsoleApp/Queries/Feed.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using TheSocialNetworkConsoleApp.Models;
+namespace TheSocialNetworkConsoleApp.Queries
+{
+    class Feed
+    {
+        public Feed(Collection collection)
+        {
+            _collection = collection;
+        }
+
+        public List<Post> 
+
+        /*Vores egne: */
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string FeedID { get; set; }
+
+        public string Logged_In_User_Id { get; set; }
+
+        public User UsersFeed { get; set; }
+    }
+}
