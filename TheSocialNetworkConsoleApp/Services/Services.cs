d15c0348cc54b41af99147ce60a4e1a8404aa011
Nikolaj Tam Binh Huynh <au591235@uni.au.dk>
2020-05-04 17:56:29 +0200

    Added CRUDS for circle and user in services

--- TheSocialNetworkConsoleApp/Services/Services.cs
+++ TheSocialNetworkConsoleApp/Services/Services.cs
@@ -23,8 +23,68 @@ namespace TheSocialNetworkConsoleApp.Services
             creationTasks.Add(_database.CreateCollectionAsync("Users"));
         }
 
+        public void retrieveCollections()
+        {
+            _user = _database.GetCollection<User>("Users");
+            _circle = _database.GetCollection<Circle>("Circles");
+        }
+
+        //********************Services for User collection********************
+
+        public List<User> GetUser()
+        {
+            return _user.Find(user => true).ToList();
+        }
+
+        public User GetUser(string id)
+        {
+            return _user.Find<User>(user => user.UserId == id).FirstOrDefault();
+        }
+
+        public User CreateUser(User NewUser)
+        {
+            _user.InsertOne(NewUser);
+            return NewUser;
+        }
+
+        public void UpdateUser(string id, User userIn) =>
+            _user.ReplaceOne(user => user.UserId == id, userIn);
+
+        public void RemoveUser(User userIn) =>
+            _user.DeleteOne(user => user.UserId == userIn.UserId);
+
+        public void RemoveUser(string id) =>
+            _user.DeleteOne(user => user.UserId == id);
 
 
+        //********************Services for Circle collection********************
+
+        //Different get methods************************
+        public List<Circle> GetCircle() => _circle.Find(circle => true).ToList();
+
+        public Circle GetCirle(string circleId) =>
+            _circle.Find<Circle>(circle => circle.CircleId == circleId).FirstOrDefault();
+
+
+
+        //Different create methods************************
+        public Circle CreateCircle(Circle NewCircle)
+        {
+            _circle.InsertOne(NewCircle);
+            return NewCircle;
+        }
+
+        //Different Update methods************************
+        public void UpdateCircle(string circleId, Circle circleIn) =>
+            _circle.ReplaceOne(circle => circle.CircleId == circleId, circleIn);
+
+
+        //Different Remove methods************************
+        public void RemoveCircle(Circle circleIn) =>
+            _circle.DeleteOne(circle => circle.CircleId == circleIn.CircleId);
+
+        public void RemoveCirle(string circleId) =>
+            _circle.DeleteOne(circle => circle.CircleId == circleId);
 
 
 
