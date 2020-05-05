bfa40122d61417adca634f8b8b716ef2b45d438c
Quynh Trang Mac <201810269@post.au.dk>
2020-05-05 15:32:28 +0200

    Added user

--- TheSocialNetworkConsoleApp/DummyData.cs
+++ TheSocialNetworkConsoleApp/DummyData.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using TheSocialNetworkConsoleApp.Queries;
+using TheSocialNetworkConsoleApp.Models;
+using TheSocialNetworkConsoleApp.Services;
 
 namespace TheSocialNetworkConsoleApp
 {
@@ -18,7 +20,22 @@ namespace TheSocialNetworkConsoleApp
 
         public void seedData()
         {
+            var henrik = _services.CreateUser(new User { UserName = "Henrik", Age = 22, Gender = "Male" });
+            henrik.CircleId.Add(Studieven.CircleId);
+            
+            var odin = _services.CreateUser(new User { UserName = "Odin", Age = 20, Gender = "Male" });
+            odin.CircleId.Add(Studieven.CircleId);
+            
+            var ninna = _services.CreateUser(new User { UserName = "Ninna", Age = 25, Gender = "Female" });
 
+            var ida = _services.CreateUser(new User { UserName = "Ida", Age = 21, Gender = "Female" });
+            ida.CircleId.Add(Studieven.CircleId);
+
+
+            _services.UpdateUser(henrik.UserId, henrik);
+            _services.UpdateUser(odin.UserId, odin);
+            _services.UpdateUser(ninna.UserId, ninna);
+            _services.UpdateUser(ida.UserId, ida);
         }
 
     }
