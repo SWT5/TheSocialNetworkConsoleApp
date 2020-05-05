aac18b72b5b5cafba42271755d60d4ff01965f51
Quynh Trang Mac <201810269@post.au.dk>
2020-05-05 15:39:05 +0200

    All users

--- TheSocialNetworkConsoleApp/DummyData.cs
+++ TheSocialNetworkConsoleApp/DummyData.cs
@@ -26,16 +26,37 @@ namespace TheSocialNetworkConsoleApp
             var WildWest = _services.CreateCircle(new Circle {CircleName = "WildWest"});
             var folketinget = _services.CreateCircle(new Circle {CircleName = "folketinget"});
             var LoyaltoFamilia = _services.CreateCircle(new Circle {CircleName = "LoyaltoFamilia"});
+            
+            
             var henrik = _services.CreateUser(new User { UserName = "Henrik", Age = 22, Gender = "Male" });
-            henrik.CircleId.Add(Studieven.CircleId);
+            henrik.Circles.Add(studieven.CircleId);
             
             var odin = _services.CreateUser(new User { UserName = "Odin", Age = 20, Gender = "Male" });
-            odin.CircleId.Add(Studieven.CircleId);
-            
+            odin.Circles.Add(studieven.CircleId);
+            odin.Circles.Add(coronagruppen.CircleId);
+            odin.Circles.Add(WildWest.CircleId);
+
+
             var ninna = _services.CreateUser(new User { UserName = "Ninna", Age = 25, Gender = "Female" });
+            ninna.Circles.Add(folketinget.CircleId);
+            ninna.Circles.Add(LoyaltoFamilia.CircleId);
 
             var ida = _services.CreateUser(new User { UserName = "Ida", Age = 21, Gender = "Female" });
-            ida.CircleId.Add(Studieven.CircleId);
+            ida.Circles.Add(folketinget.CircleId);
+            ida.Circles.Add(LoyaltoFamilia.CircleId);
+            ida.Circles.Add(coronagruppen.CircleId);
+
+            var svend = _services.CreateUser(new User { UserName = "Svend", Age = 27, Gender = "Male" });
+            svend.Circles.Add(folketinget.CircleId);
+            svend.Circles.Add(LoyaltoFamilia.CircleId);
+            svend.Circles.Add(coronagruppen.CircleId);
+
+            var vilde = _services.CreateUser(new User { UserName = "Vilde", Age = 24, Gender = "Female" });
+            vilde.Circles.Add(folketinget.CircleId);
+            vilde.Circles.Add(LoyaltoFamilia.CircleId);
+            vilde.Circles.Add(coronagruppen.CircleId);
+            vilde.Circles.Add(WildWest.CircleId);
+            vilde.Circles.Add(studieven.CircleId);
 
 
             _services.UpdateUser(henrik.UserId, henrik);
@@ -43,12 +64,8 @@ namespace TheSocialNetworkConsoleApp
             _services.UpdateUser(ninna.UserId, ninna);
             _services.UpdateUser(ida.UserId, ida);
         }
-            var studieven = _services.CreateCircle(new Circle{CircleName = "Studieven"});
-            var coronagruppen = _services.CreateCircle(new Circle {CircleName = "coronagruppen" });
-            var WildWest = _services.CreateCircle(new Circle { CircleName = "WildWest" });
-            var folketinget = _services.CreateCircle(new Circle { CircleName = "folketinget" });
-            var LoyaltoFamilia = _services.CreateCircle(new Circle { CircleName = "LoyaltoFamilia" });
+           
 
-        }
+        
     }
 }
